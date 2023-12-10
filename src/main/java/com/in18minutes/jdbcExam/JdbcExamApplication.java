32375d7466c2dee783973d1e433e878a3b2cfdad
Mercia Jeno <mercia.jeno@gmail.com>
2023-12-10 18:29:57 +0530

    Initial commit

---
+++ src/main/java/com/in18minutes/jdbcExam/JdbcExamApplication.java
@@ -0,0 +1,13 @@
+package com.in18minutes.jdbcExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/in18minutes/jdbcExam/JdbcExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in18minutes.jdbcExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
